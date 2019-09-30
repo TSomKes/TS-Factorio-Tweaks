2865b640d567e8ef7df5e77893d27183d9c8a38a
TSomKes <tsomkes@gmail.com>
2019-09-30 11:46:51 -0500

    Add uranium, refinery & centrifuge to Extraction

--- ts_tweaks/prototypes/extraction.lua
+++ ts_tweaks/prototypes/extraction.lua
@@ -8,12 +8,13 @@ local g = "ts-tab-extraction"
 data:extend({
 	{ type = "item-subgroup", group = g, name = "ts-ext-miners", order = "a" },
 	
-	{ type = "item-subgroup", group = g, name = "ts-ext-raw", order = "b" },
+	{ type = "item-subgroup", group = g, name = "ts-ext-refineries", order = "b" },
+
+	{ type = "item-subgroup", group = g, name = "ts-ext-materials", order = "c" },
 })
 
 
 local subgroup
-
 
 subgroup = "ts-ext-miners"
 place(subgroup, "burner-mining-drill", "a")
@@ -21,9 +22,15 @@ place(subgroup, "electric-mining-drill", "a")
 place(subgroup, "pumpjack", "c")
 place(subgroup, "offshore-pump", "d")
 
-subgroup = "ts-ext-raw"
+subgroup = "ts-ext-refineries"
+place(subgroup, "oil-refinery", "a")
+place(subgroup, "centrifuge", "b")
+
+subgroup = "ts-ext-materials"
 place(subgroup, "coal", "a")
 place(subgroup, "stone", "b")
 place(subgroup, "iron-ore", "c")
 place(subgroup, "copper-ore", "d")
 place(subgroup, "uranium-ore", "e")
+place(subgroup, "uranium-238", "f")
+place(subgroup, "uranium-235", "g")
