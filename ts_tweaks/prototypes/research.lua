f627989b72cebb7dca6b0190e5b3b1ca094c2690
TSomKes <tsomkes@gmail.com>
2019-10-03 08:12:45 -0500

    Add support for Bob's Technology

--- ts_tweaks/prototypes/research.lua
+++ ts_tweaks/prototypes/research.lua
@@ -8,7 +8,8 @@ local g = "ts-tab-research"
 data:extend({
 	{ type = "item-subgroup", group = g, name = "ts-research-labs", order = "a" },
 	
-	{ type = "item-subgroup", group = g, name = "ts-research-science-packs", order = "b" },
+	{ type = "item-subgroup", group = g, name = "ts-research-science-packs", order = "ba" },
+	{ type = "item-subgroup", group = g, name = "ts-research-science-packs-bob", order = "bb" },
 })
 
 
@@ -17,8 +18,10 @@ local subgroup
 subgroup = "ts-research-labs"
 place(subgroup, "burner-lab", "a")
 place(subgroup, "lab", "b")
-place(subgroup, "lab-module", "c")
-place(subgroup, "rocket-silo", "d")
+place(subgroup, "lab-2", "c")
+place(subgroup, "lab-module", "d")
+place(subgroup, "lab-alien", "e")
+place(subgroup, "rocket-silo", "f")
 
 subgroup = "ts-research-science-packs"
 place(subgroup, "automation-science-pack", "a")
@@ -29,3 +32,14 @@ place(subgroup, "production-science-pack", "e")
 place(subgroup, "utility-science-pack", "f")
 place(subgroup, "space-science-pack", "g")
 place(subgroup, "satellite", "h")
+
+subgroup = "ts-research-science-packs-bob"
+place(subgroup, "advanced-logistic-science-pack", "a")
+place(subgroup, "science-pack-gold", "b")
+place(subgroup, "alien-science-pack", "c")
+place(subgroup, "alien-science-pack-blue", "d")
+place(subgroup, "alien-science-pack-orange", "e")
+place(subgroup, "alien-science-pack-purple", "f")
+place(subgroup, "alien-science-pack-yellow", "g")
+place(subgroup, "alien-science-pack-green", "h")
+place(subgroup, "alien-science-pack-red", "i")
